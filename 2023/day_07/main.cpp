bb4ef55e7c1f088d5a28b5a37667500876eaed6b
Tim Granger <tim@mcshane.org>
2026-01-20 18:46:54 +0000

    Add day 7 part 2

--- 2023/day_07/main.cpp
+++ 2023/day_07/main.cpp
@@ -11,6 +11,7 @@ struct counting {
     uint max_run;
     uint next_run;
     uint current_run;
+    uint wildcards;
     char last_char;
 };
 
@@ -38,10 +39,44 @@ uint score_card(std::string card) {
     uint sum = std::accumulate(card.begin(), card.end(), 0, [&score](uint lhs, char rhs) { return (lhs<<4) + score[rhs]; });
 
     std::sort(card.begin(), card.end());
-    struct counting rec = {0,0,0,0};
+    struct counting rec = {0,0,0,0,0};
     struct counting rec2 = std::accumulate(card.begin(), card.end(), rec, count_reps_op);
     
     return (rec2.max_run << 24) | (rec2.next_run << 20) | sum;
+}
+
+struct counting count_reps_wildcards_op(struct counting& lhs, char rhs) {
+    if(rhs == 'J') {
+        lhs.wildcards++;
+    } else {
+        if(lhs.last_char == rhs) {
+            lhs.current_run++;
+        } else {
+            lhs.current_run = 1;
+            lhs.last_char = rhs;
+        }
+
+        if(lhs.current_run > lhs.max_run) {
+            lhs.max_run = lhs.current_run;
+        } else if (lhs.current_run > lhs.next_run) {
+            lhs.next_run = lhs.current_run;
+        }
+    }
+
+    return lhs;
+}
+
+uint score_wildcards(std::string card) {
+    std::map<char,uint> score = {{'J',1}, {'2',2}, {'3',3}, {'4',4}, {'5',5}, {'6',6}, {'7',7}, {'8',8},
+                                {'9',9}, {'T',10},{'Q',12}, {'K',13}, {'A',14}};
+
+    uint sum = std::accumulate(card.begin(), card.end(), 0, [&score](uint lhs, char rhs) { return (lhs<<4) + score[rhs]; });
+
+    std::sort(card.begin(), card.end());
+    struct counting rec = {0,0,0,0,0};
+    struct counting rec2 = std::accumulate(card.begin(), card.end(), rec, count_reps_wildcards_op);
+
+    return ((rec2.max_run + rec2.wildcards) << 24) | (rec2.next_run << 20) | sum;
 }
 
 uint64_t total_winnings(std::vector<std::pair<uint,uint>>& cards) {
@@ -62,6 +97,7 @@ int main(int argc, char *argv[]) {
     std::string line;
 
     std::vector<std::pair<uint,uint>> cards;
+    std::vector<std::pair<uint,uint>> cards_wildcards;
 
     while(std::getline(input, line)) {
         std::stringstream ss(line);
@@ -70,9 +106,11 @@ int main(int argc, char *argv[]) {
         ss >> wager;
 
         cards.emplace_back(score_card(card), std::stoul(wager));
+        cards_wildcards.emplace_back(score_wildcards(card), std::stoul(wager));
     }
 
     std::cout << "Part1: " << total_winnings(cards) << "\n";
+    std::cout << "Part2: " << total_winnings(cards_wildcards) << "\n";
     
     return 0;
 }
