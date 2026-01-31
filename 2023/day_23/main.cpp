69e3afa43facb3415d2a28d60028a86b20a6d1ab
Tim Granger <tim@mcshane.org>
2026-01-31 16:40:49 +0000

    Add day 23 part 2

--- 2023/day_23/main.cpp
+++ 2023/day_23/main.cpp
@@ -8,65 +8,118 @@
 struct pos {
     int x;
     int y;
-    std::string path;
     int prev_x;
     int prev_y;
     int length;
 };
 
-int longest_path(std::vector<std::string>& map) {
-    int longest = 0;
-    int xd[] = {1, 0, -1, 0};
-    int yd[] = {0, -1, 0, 1};
-    std::map<char,int> slope = {{'>',0}, {'^',1}, {'<',2}, {'v',3}, {'#',-1}};
-    std::vector<struct pos> frontier;
-    std::vector<std::vector<std::set<std::string>>> visited(map.size(),std::vector<std::set<std::string>>(map[0].length()));
+struct node {
+    int x;
+    int y;
+    std::vector<std::pair<int,int>> next;
+    bool last;
+};
 
-    frontier.push_back({1, 0, "S", 0, 0, 0});
-    while(frontier.size() > 0) {
-        auto &p = frontier.back();
-        frontier.pop_back();
-
-        //std::cout << "going from " << p.x << "," << p.y << "," << p.path << "\n";
-        auto it = std::find_if(visited[p.y][p.x].begin(), visited[p.y][p.x].end(),
-                [p](const std::string& path) { return path.length() <= p.path.length() && 
-                                                p.path.substr(0,path.length()) == path; });
-        if(it != visited[p.y][p.x].end()) {
-            //std::cout << " - found subset visited:" << (*it) << " current " << p.path << "\n";
-            continue;
-        }        
-
-        visited[p.y][p.x].insert(p.path);
-
-        if(p.y == map.size()-1 && p.x == map[p.y].length()-2) {
-            longest = std::max(longest, p.length);
-            //std::cout << "reached end with " << p.path << " length " << p.length << "\n";
-            continue;
-        }
-
-        std::vector<struct pos> options;
-        for(int dir=0; dir<4; dir++) {
-            int ny = p.y+yd[dir];
-            int nx = p.x+xd[dir];
-            if(ny >= 0 && ny < map.size() && nx >= 0 && nx < map[ny].length() && !(ny == p.prev_y && nx == p.prev_x)) {
-                if(map[ny][nx] == '.' || slope[map[ny][nx]] == dir) {
-                    options.push_back({nx, ny, p.path, p.x, p.y, p.length+1});
-                    //std::cout << " - option to go to " << p.x << "," << p.y << "\n";
-                }
-            }
-        }
-        if(options.size() > 1) {
-            for(int i=0; i<options.size(); i++) {
-                std::string ex(1, '0'+i);   
-                frontier.push_back({options[i].x, options[i].y,
-                                    options[i].path.append(ex), p.x, p.y, options[i].length});
-            }
-        } else if(options.size() == 1) {
-            frontier.push_back(options[0]);
+int explore_from(std::vector<struct node>& nodes, int from, int length, std::set<int>& path) {
+    if(nodes[from].last) {
+        //std::cout << "Reached last in " << length << "\n";
+        return length;
+    }
+    //std::cout << "exploring from " << from << " in " << length << "\n";
+    int max_length = 0;
+    for(auto &p : nodes[from].next) {
+        if(!path.contains(p.first)) {
+            //std::cout << " - going to " << p.first << "\n";
+            path.insert(from);
+            max_length = std::max(max_length, explore_from(nodes, p.first, length + p.second, path));
+            path.erase(from);
         }
     }
 
-    return longest;
+    return max_length;
+}
+
+int find_nodes(std::vector<std::string>& map, bool slippery = true) {
+    int xd[] = {1, 0, -1, 0};
+    int yd[] = {0, -1, 0, 1};
+    std::map<char,int> slope = {{'>',0}, {'^',1}, {'<',2}, {'v',3}, {'#',-1}};
+    std::set<char> walkable = {'.'};
+    std::vector<struct node> nodes;
+    std::vector<int> explore;
+
+    if(!slippery) {
+        walkable.insert('<');
+        walkable.insert('^');
+        walkable.insert('v');
+        walkable.insert('>');
+    }   
+
+    nodes.push_back({1,0,std::vector<std::pair<int,int>>(), false});
+    explore.push_back(0);
+
+    while(explore.size() > 0) {
+        auto n = explore.back();
+        explore.pop_back();
+
+        //std::cout << "Exploring from node " << n << " at " << nodes[n].x << "," << nodes[n].y << "\n";
+
+        std::vector<struct pos> options;
+        for(int dir=0; dir<4; dir++) {
+            int ny = nodes[n].y + yd[dir];
+            int nx = nodes[n].x + xd[dir];
+            if(ny >= 0 && ny < map.size() && nx >= 0 && nx < map[ny].length()) {
+                if(walkable.contains(map[ny][nx]) || slope[map[ny][nx]] == dir) {
+                    options.push_back({nx, ny, nodes[n].x, nodes[n].y, 1});
+                }
+            }
+        }
+
+        for(auto p : options) {
+            while(true) {
+                if(p.y == map.size()-1 && p.x == map[p.y].length()-2) {
+                    break;
+                }
+                if(p.x == 1 && p.y == 0) {
+                    break;
+                }
+
+                std::vector<struct pos> next_options;
+                for(int dir=0; dir<4; dir++) {
+                    int ny = p.y + yd[dir];
+                    int nx = p.x + xd[dir];
+                    if(ny >= 0 && ny < map.size() && nx >= 0 && nx < map[ny].length() && !(ny == p.prev_y && nx == p.prev_x)) {
+                        if(walkable.contains(map[ny][nx]) || slope[map[ny][nx]] == dir) {
+                            next_options.push_back({nx, ny, p.x, p.y, p.length+1});
+                        }
+                    }
+                }
+                if(next_options.size() > 1) {
+                    break;
+                }
+                if(next_options.size() == 0) {
+                    p.length = 0;
+                    break;
+                }
+                p = next_options[0];
+            }
+            if(p.length > 0) {
+                //std::cout << "  Found route to " << p.x << "," << p.y << " length " << p.length << "\n";
+                auto it = std::find_if(nodes.begin(), nodes.end(), [&p](const struct node& n){ return n.x == p.x && n.y == p.y; });
+                if(it == nodes.end()) {
+                    //std::cout << "  - adding new node " << nodes.size() << "\n";
+                    explore.push_back(nodes.size());
+                    nodes[n].next.emplace_back(nodes.size(), p.length);
+                    nodes.emplace_back(p.x, p.y, std::vector<std::pair<int,int>>(), p.y == map.size()-1 && p.x == map[p.y].length()-2);
+                } else {
+                    //std::cout << "  - found node " << (it - nodes.begin()) << "\n";
+                    nodes[n].next.emplace_back(it - nodes.begin(), p.length);
+                }
+            }
+        }
+    }
+
+    std::set<int> path;
+    return explore_from(nodes, 0, 0, path);
 }
 
 int main(int argc, char *argv[]) {
@@ -78,9 +131,8 @@ int main(int argc, char *argv[]) {
         map.push_back(line);
     }
 
-    std::cout << "Part1: " << longest_path(map) << "\n";
+    std::cout << "Part1: " << find_nodes(map) << "\n";
+    std::cout << "Part2: " << find_nodes(map, false) << "\n";
 
     return 0;
 }
-
-
