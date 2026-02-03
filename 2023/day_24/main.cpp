8fe5324dcbac5e58f08fc7f13573fb2de5f36f77
Tim Granger <tim@mcshane.org>
2026-02-03 19:50:27 +0000

    Add analysis for day 24 part 2

--- 2023/day_24/main.cpp
+++ 2023/day_24/main.cpp
@@ -5,14 +5,12 @@
 #include <map>
 #include <set>
 #include <regex>
+#include <numeric>
 
 struct hailstone {
-    int64_t x;
-    int64_t y;
-    int64_t z;
-    int64_t dx;
-    int64_t dy;
-    int64_t dz;
+    int num;
+    int64_t p[3];
+    int64_t a[3];
 };
 
 struct collision {
@@ -33,17 +31,17 @@ struct collision find_collision(struct hailstone &a, struct hailstone &b) {
     // a.y + (a.dy * ta) = b.y + (b.dy / b.dx) * ((a.x - b.x) + (a.dx * ta))
     // a.y - b.y - (b.dy / b.dx) * (a.x - b.x) = ta * (((b.dy / b.dx) * a.dx) - a.dy)
 
-    if(b.dx == 0) {
+    if(b.a[0] == 0) {
         return {false, 0.0, 0.0, 0.0};
     }
-    double bdyx = (double) b.dy / (double) b.dx;
-    double lhs = (double) a.y - b.y - bdyx * (a.x - b.x);
-    double rhs = (bdyx * a.dx) - (double) a.dy;
+    double bdyx = (double) b.a[1] / (double) b.a[1];
+    double lhs = (double) a.p[1] - b.p[1] - bdyx * (a.p[0] - b.p[0]);
+    double rhs = (bdyx * a.a[0]) - (double) a.a[1];
     if(rhs == 0.0) {
         return {false, 0.0, 0.0, 0.0};
     }
     double ta = lhs / rhs;
-    return {true, ta, (double) a.x + (a.dx * ta), (double) a.y + (a.dy * ta)};
+    return {true, ta, (double) a.p[0] + (a.a[0] * ta), (double) a.p[1] + (a.a[1] * ta)};
 }
 
 int count_collisions(std::vector<struct hailstone>& hailstones, double min, double max) {
@@ -72,6 +70,41 @@ int count_collisions(std::vector<struct hailstone>& hailstones, double min, doub
     return total;
 }
 
+void analysis(std::vector<struct hailstone>& hailstones) {
+    for(int axis=0; axis<3; axis++) {
+        std::sort(hailstones.begin(), hailstones.end(), [axis](const hailstone& a, const hailstone& b) {return a.p[axis] < b.p[axis]; });
+
+        std::vector<int64_t> amin(hailstones.size()+1);
+        amin[0] = INT64_MAX;
+        std::vector<int64_t> amax(hailstones.size()+1);
+        amax[hailstones.size()] = INT64_MIN;
+        std::transform(hailstones.begin(), hailstones.end(), amin.begin(), std::next(amin.begin()), [axis](const hailstone& h, const int64_t m) { return std::min(m, h.a[axis]); });
+        std::transform(hailstones.rbegin(), hailstones.rend(), amax.rbegin(), std::next(amax.rbegin()), [axis](const hailstone& h, const int64_t m) { return std::max(m, h.a[axis]); });
+
+        std::cout << "\n" << axis << "\n";
+        std::cout << "     position in [* .. " << (hailstones[0].p[axis]-1) << "] needs velocity in [" << (amax[0]+1) << " .. *]\n";
+        for(int i=0; i<hailstones.size(); i++) {
+            //std::cout << "  " << i << " num:" << hailstones[i].num << " p:" << hailstones[i].p[axis] << " a:" << hailstones[i].a[axis] << " min:" << amin[i+1] <<  " rmax:" << amax[i] << " " << (amax[i] > amin[i+1] ? "invalid" : "valid") << "\n";
+
+            if(i > 0 && i < hailstones.size()-1) {
+                if(amin[i] > amax[i+1]+1) {
+                    std::cout << "     position equal to " << hailstones[i].p[axis] << " needs velocity in [" << (amax[i+1]+1) << " .. " << (amin[i]-1) << "]\n";
+                }
+            } else if(i == 0) {
+                std::cout << "     position equal to " << hailstones[i].p[axis] << " needs velocity in [" << (amax[i+1]+1) << " .. *]\n";
+            } else {
+                std::cout << "     position equal to " << hailstones[i].p[axis] << " needs velocity in [* .. " << (amin[i]-1) << "]\n";
+                break;
+            }
+            if(amin[i+1] > amax[i+1]+1) {
+                std::cout << "     position in [" << (hailstones[i].p[axis]+1) << " .. " << (hailstones[i+1].p[axis]-1) << "] (len " << (hailstones[i+1].p[axis] - hailstones[i].p[axis] -1)
+                            << ") needs velocity in [" << (amax[i+1]+1) << " .. " << (amin[i+1]-1) << "] (len " << (amin[i+1]-amax[i+1]-1) << ")\n";
+            }
+        }
+        std::cout << "     position in [" << (hailstones.back().p[axis]+1) << " .. *] needs velocity in [* .. " << (amin[hailstones.size()]-1) << "]\n";
+    }
+}
+
 int main(int argc, char *argv[]) {
     std::ifstream input(argc >= 2 ? argv[1] : "test_input.txt");
     std::string line;
@@ -82,12 +115,18 @@ int main(int argc, char *argv[]) {
     while(std::getline(input, line)) {
         if(std::regex_match(line, res, pat)) {
             //std::cout << res[1] << " " << res[2] << " " << res[3] << " | " << res[4] << " " << res[5] << " " << res[6] << "\n";
-            hailstones.emplace_back(std::stoll(res[1]), std::stoll(res[2]), std::stoll(res[3]),
-                                    std::stoll(res[4]), std::stoll(res[5]), std::stoll(res[6]));
+            struct hailstone h;
+            for(int i=0; i<3; i++) {
+                h.p[i] = std::stoll(res[i+1]);
+                h.a[i] = std::stoll(res[4+i]);
+            }
+            h.num = hailstones.size();
+            hailstones.push_back(h);
         }
     }
 
-    std::cout << "Part1: " << count_collisions(hailstones, 200000000000000, 400000000000000) << "\n";
+    //std::cout << "Part1: " << count_collisions(hailstones, 200000000000000, 400000000000000) << "\n";
+    analysis(hailstones);
 
     return 0;
 }
