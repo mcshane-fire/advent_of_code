5d0413ad8140daafd5d2fcdda61991854604c072
Tim Granger <tim@mcshane.org>
2026-06-05 01:52:05 -0700

    Add day 24 speculative part2

--- 2020/day_24/main.cpp
+++ 2020/day_24/main.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
+
+enum Direction {
+    EAST,
+    WEST,
+    NORTH_EAST,
+    NORTH_WEST,
+    SOUTH_EAST,
+    SOUTH_WEST,
+};
 
 struct Pos {
     int x = 0;
@@ -15,23 +25,21 @@ struct Pos {
 
     auto operator<=>(const Pos &p) const = default;
 
-    void move(std::string ins, int &used) {
-        if(ins[0] == 'e') {
-            used = 1;
-            x++;
-        } else if(ins[0] == 'w') {
-            used = 1;
-            x--;
+    Pos move(Direction d) const {
+        Pos ret;
+        if(d == EAST) {
+            ret = Pos(x+1, y);
+        } else if(d == WEST) {
+            ret = Pos(x-1, y);
         } else {
-            int ver = ins[0] == 's' ? 1 : -1;
-            int hor = ins[1] == 'e' ? 1 : -1;
-            used = 2;
+            int ver = d == SOUTH_EAST || d == SOUTH_WEST ? 1 : -1;
+            int hor = d == SOUTH_EAST || d == NORTH_EAST ? 1 : -1;
             if(((y & 1) == 1 && hor == -1) || ((y & 1) == 0 && hor == 1)) {
                 hor = 0;
             }
-            x += hor;
-            y += ver;
+            ret = Pos(x+hor, y+ver);
         }
+        return ret;
     }
 
     friend std::ostream& operator<<(std::ostream &os, const Pos &p) {
@@ -40,21 +48,46 @@ struct Pos {
     }
 };
 
-int count_black(std::vector<std::string> &paths) {
+int count_black(std::vector<std::string> &paths, int days = 0) {
     std::set<Pos> black;
+    std::map<std::string, Direction> sd = {
+        {"e", EAST}, {"w", WEST},
+        {"ne", NORTH_EAST}, {"nw", NORTH_WEST},
+        {"se", SOUTH_EAST}, {"sw", SOUTH_WEST}};
 
     for(auto s : paths) {
         Pos p = Pos(0,0);
         while(s.length() > 0) {
-            int used;
-            p.move(s, used);
-            s = s.substr(used);
+            if(sd.contains(s.substr(0,1))) {
+                p = p.move(sd[s.substr(0,1)]);
+                s = s.substr(1);
+            } else {
+                p = p.move(sd[s.substr(0,2)]);
+                s = s.substr(2);
+            }
         }
         if(black.contains(p)) {
             black.erase(p);
         } else {
             black.insert(p);
         }
+    }
+
+    for(int i=0; i<days; i++) {
+        std::map<Pos,int> adj;
+        for(auto &p : black) {
+            for(auto &d : sd) {
+                adj[p.move(d.second)]++;
+            }
+        }
+        std::set<Pos> next;
+        for(auto &p : adj) {
+            if(p.second == 2 || (p.second == 1 && black.contains(p.first))) {
+                next.insert(p.first);
+            }
+        }
+        black.clear();
+        black.swap(next);
     }
 
     return black.size();
@@ -72,6 +105,7 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << "Part1: " << count_black(paths) << "\n";
+    std::cout << "Part2: " << count_black(paths, 100) << "\n";
 
     return 0;
 }
